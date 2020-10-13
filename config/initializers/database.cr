3a16a111e6ed0ed6312d97238f612f8de5556809
Anton Maminov <anton.linux@gmail.com>
2020-10-13 13:02:12 +0300

    update for latest Crystal

--- config/initializers/database.cr
+++ config/initializers/database.cr
@@ -4,4 +4,5 @@ require "../../src/models/**"
 require "../../src/db/seeds"
 
 Clear::SQL.init("postgres://postgres@localhost/kemal_and_clear_sample_app", connection_pool_size: 5)
-Clear.logger.level = ::Logger::DEBUG
+
+Log.builder.bind "clear.*", :debug, Log::IOBackend.new(STDOUT)
