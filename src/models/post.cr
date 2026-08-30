ac2d64392963598201c480db3544e2480090dd3a
Anton Maminov <anton.linux@gmail.com>
2026-08-30 15:03:16 +0300

    remove unused code

--- src/models/post.cr
+++ src/models/post.cr
@@ -16,22 +16,4 @@ class Post
 
   has_many dependencies : Post, through: Relationship, foreign_key: "follower_id", own_key: "leader_id"
   has_many dependents : Post, through: Relationship, foreign_key: "leader_id", own_key: "follower_id"
-
-  def tags=(names : Array(String))
-    names.map do |name|
-      tag = Tag.query.find_or_create(name: name)
-      tags << tag
-    end
-
-    unlink_tags = tag_names - names
-    unlink_tags.each do |name|
-      if tag = Tag.find_by!(name: name)
-        tags.unlink(tag)
-      end
-    end
-  end
-
-  def tag_names
-    tags.map(&.name)
-  end
 end
